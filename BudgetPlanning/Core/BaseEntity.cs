3fda66cff2e9f0c7e3c87d2b2aa7b3e59d5d6c0b
Alexander Novikov <a.novikov@mst.su>
2020-11-04 06:43:10 +0300

    init project

---
+++ BudgetPlanning/Core/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core
+{
+    /// <summary>
+    /// Base class for entities
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the entity identifier
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
